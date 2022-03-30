a64cf108b627001d1839fad41e019a44b1682bc0
= <=>
2022-03-30 21:37:12 +0800

    空字符串,增加了size,[]cost成员访问,

--- STL/stl/string/mystring/mystring.h
+++ STL/stl/string/mystring/mystring.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string.h>
 #include <iostream>
+#include<cassert>
 using namespace std;
 namespace xzw
 {
@@ -8,19 +9,32 @@ namespace xzw
     {
     private:
         char *_str;
-        size_t size;
-        size_t capacit;
-
+        size_t _size;
+        size_t _capacity; //有效空间个数，不算上\0
     public:
-        string(const char *str) //构造函数
+        // string()//构造一个空字符串
+        //     :_str(new char[1])
+        //     ,_size(0)
+        //     ,_capacity(0)//存0个有效，
+        // {
+        //     _str[0]='\0';
+        // }
+
+        string(const char *str= "" ) //构造函数，我们可以给一个全却缺省
             //: _str(str)//不能这样写，因为我们要能够动态的扩容
-            : _str(new char[strlen(str) + 1]) //我们要new才可以动态的扩容，
+            : _size(strlen(str)), _capacity(_size)
         {
+            _str = new char[_capacity + 1]; //增容就是相等的时候就扩容
             strcpy(_str, str);
         }
-        string(const string &s) //传引用拷贝
-            : _str(new char[strlen(s._str) + 1])
+        const char *c_str()
         {
+            return _str;
+        }
+        string(const string &s) //传引用拷贝
+            : _size(s._size), _capacity(s._capacity)
+        {
+            _str = new char[_capacity + 1];
             strcpy(_str, s._str);
         }
         //assign，赋值运算符
@@ -34,20 +48,41 @@ namespace xzw
             //     _str = new char[strlen(s._str) + 1];//开一个和s一样大的空间，
             //     strcpy(_str, s._str);
             // }
-                 if (this!=&s)
+            if (this != &s)
             {
-                char* tmp=new char[strlen(s._str)+1];
-                strcpy(tmp,s._str);//先拷贝再释放掉
-                delete [] _str;//
-                _str=tmp;
+                char *tmp = new char[+1];
+                strcpy(tmp, s._str); //先拷贝再释放掉
+                delete[] _str;       //
+
+                _str = tmp;
+                _size = s._size;
+                _capacity = s._capacity;
             }
-            return *this;//出了作用于*this还在，所以传引用返回
+            return *this; //出了作用于*this还在，所以传引用返回
         }
         ~string() //析构函数
         {
             delete[] _str; //new完之后就要delete
             _str = nullptr;
+            _size = _capacity = 0;
         }
+
+        size_t size() const//遍历string,const在后面，则this的东西都不能够修改
+        {
+            return _size;
+        }
+        char& operator[](size_t pos) //重载，返回其引用
+        {
+            assert(pos<_size);
+            return _str[pos];//出了作用域，还存在,并且我们能够对其进行修改，this的东西发生了修改
+        }
+         const char& operator[](size_t pos) const//重载,const变量，不可以改变
+        {
+            //判断是否出了范围
+            assert(pos<_size);
+            return _str[pos];//const修饰都不能改变
+        }
+
     };
     void test_string1()
     {
@@ -58,7 +93,24 @@ namespace xzw
         string s3("hello ");
         s = s3; //如果我们不写就是浅拷贝，连着地址一起拷贝过去了，s内存还没有释放，内存泄漏
         //我们不能直接去拷贝，因为可能会出现空间不够，导致还要去进一步的扩容，小拷贝到大，会导致空间用不完，
-        s="alcd";
+        s = "alcd";
         printf("sdahello,world a\n");
+    }
+    void test_string2()
+    {
+        string s("hello world");
+ 
+        string s1; //不带参数
+        cout<<s.c_str()<<endl;//c形式的输出，
+        cout<<s1.c_str()<<endl;//遇到\0就结束,\0是不可见的字符
+        s[0]='d';
+        //s[20]='a';
+        for(size_t i=0;i<s.size();i++)
+        {
+            cout<<s[i]<<" ";
+        }
+
+        cout<<endl;
+
     }
 }
