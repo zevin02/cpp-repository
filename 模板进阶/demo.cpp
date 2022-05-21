a3ea79244950f141749222e51c90e455a961549b
zevin02 <1833195090@qq.com>
2022-05-21 22:31:45 +0800

    updata

---
+++ 模板进阶/demo.cpp
@@ -0,0 +1,8 @@
+#include"vector.h"
+
+int main()
+{
+    F(10);
+    F2("sac");
+    return 0;
+}
---
+++ 模板进阶/vector.cpp
@@ -0,0 +1,11 @@
+#include"vector.h"
+void F(int n)
+{
+    cout<<"F(int n)"<<endl;
+}
+template<class T>
+
+void F2(const T& n)
+{
+    cout<<"void f1(const t& n)"<<endl;
+}
