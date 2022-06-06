7a7b70d42ca72a66077e19b0fae54d399f4016fd
zevin02 <1833195090@qq.com>
2022-06-06 17:11:53 +0800

    学习了右值和移动构造

--- c++11/test.cpp
+++ c++11/test.cpp
@@ -177,29 +177,185 @@ void test7()
     min(x, y);
     //这几个都不能&
 
-    int &&r1 = 10;
-    double &&rr2 = x + y;
-    double &&r3 = min(x, y);
+    int &&r0 = 10;
+    double &&l = x + y;
+    // double &&r3 = min(x, y);
 
     //左值引用不能引用右值----不能直接引用，但是可以加const，左值引用
-    const int& r=10;
-    const double& e2=x+y;
-    //void pushback(const T & x)像直接传进去数字这样的右值，引用也可以实现
-
+    const int &c = 10;
+    const double &e22 = x + y;
+    // void pushback(const T & x)像直接传进去数字这样的右值，引用也可以实现
 
     //右值引用不能引用左值，那么也是引用move 后的左值
-    int && r=move(a);
+    int &&r = move(a);
 
     //所以是可以交叉引用的
-
+    //需要注意的是，右值不能取地址，给右值取别名之后，又变成了左值,因为在特定的地方把他存起来了，
 }
 
+#include <assert.h>
+#include <cstring>
+//右值引用的产生是为了弥补左值引用的不足
+
+namespace bit
+{
+    class string
+    {
+    public:
+        typedef char *iterator;
+        iterator begin()
+        {
+            return _str;
+        }
+        iterator end()
+        {
+            return _str + _size;
+        }
+        string(const char *str = "")
+            : _size(strlen(str)), _capacity(_size)
+        {
+            // cout << "string(char* str)" << endl;
+            _str = new char[_capacity + 1];
+            strcpy(_str, str);
+        }
+        // s1.swap(s2)
+        void swap(string &s)
+        {
+            ::swap(_str, s._str);
+            ::swap(_size, s._size);
+            ::swap(_capacity, s._capacity);
+        }
+        // 拷贝构造
+        string(const string &s)//编译器会判定，如果是左值就会到这里来
+            : _str(nullptr)
+        {
+            cout << "string(const string& s) -- 深拷贝" << endl;
+            string tmp(s._str);
+            swap(tmp);
+        }
+        // 赋值重载
+        string &operator=(const string &s)
+        {
+            cout << "string& operator=(string s) -- 深拷贝" << endl;
+            string tmp(s);
+            swap(tmp);
+            return *this;
+        }
+        // 移动构造
+         // 移动构造，如果是右值就会到这里来
+        //这种将亡值资源析构了很可惜，直接给我把
+        string(string &&s) //要开始移动资源,c++11中，将右值分为1.纯右值（10，x+t，min（x，y）），2.将亡值(自定义对象)
+            : _str(nullptr), _size(0), _capacity(0)
+        {
+            cout << "string(string&& s) -- 移动语义" << endl;
+            swap(s);
+            
+        }
+        // 移动赋值
+        string &operator=(string &&s)
+        {
+            cout << "string& operator=(string&& s) -- 移动语义" << endl;
+            swap(s);//
+            return *this;
+        }
+        ~string()
+        {
+            delete[] _str;
+            _str = nullptr;
+        }
+        char &operator[](size_t pos)
+        {
+            assert(pos < _size);
+            return _str[pos];
+        }
+        void reserve(size_t n)
+        {
+            if (n > _capacity)
+            {
+                char *tmp = new char[n + 1];
+                strcpy(tmp, _str);
+                delete[] _str;
+                _str = tmp;
+                _capacity = n;
+            }
+        }
+        void push_back(char ch)
+        {
+            if (_size >= _capacity)
+            {
+                size_t newcapacity = _capacity == 0 ? 4 : _capacity * 2;
+                reserve(newcapacity);
+            }
+
+            // 左值引用的使用场景：
+            // 做参数和做返回值都可以提高效率。
+            // 左值引用的短板：
+            // 但是当函数返回对象是一个局部变量，出了函数作用域就不存在了，就不能使用左值引用返回，
+            // 只能传值返回。例如：bit::string to_string(int value)函数中可以看到，这里只能使用传值返回，
+            // 传值返回会导致至少1次拷贝构造(如果是一些旧一点的编译器可能是两次拷贝构造)。
+            _str[_size] = ch;
+            ++_size;
+            _str[_size] = '\0';
+        }
+        // string operator+=(char ch)
+        string &operator+=(char ch)
+        {
+            push_back(ch);
+            return *this;
+        }
+
+        string operator+(char ch)
+        {
+            string tmp(*this);
+            push_back(ch);
+            //把他的资源直接转移走，销毁了是空
+            return tmp; //这个东西出了作用域就销毁了，
+                        //不能用左值返回，这里的tmp是一个右值
+        }
+
+        const char *c_str() const
+        {
+            return _str;
+        }
+
+    private:
+        char *_str;
+        size_t _size;
+        size_t _capacity; // 不包含最后做标识的\0
+    };
+}
+
+// 1。左值引用左参数，可以完美的解决所有的问题
+// 2.左值引用做返回值，只能解决部分问题(出了作用域还在，没有进行拷贝)
+
+//右值引用如何解决operator+的拷贝函数
+//现在提高了一个移动构造
+void func1(bit::string s) //这个会发生拷贝构造
+{
+}
+
+void func2(const bit::string &s) //使用const的引用，既可以传递左值，也可以传递右值
+{
+}
+
+void test8()
+{
+    bit::string s("dsa");
+    s + 'd'; //会拷贝，左值引用只能解决+=，但是解决不了+，
+    //这种传值返回的函数对象，右值，将亡值，
+}
+bit::string func()
+{
+    bit::string x("dasd");
+    return x;//这里会先拷贝再析构是一种资源浪费，移动构造将x识别为右值，直接调用移动构造转移资源，提高效率
+}
 int main()
 {
     test1();
     test2();
     test3();
     test4();
-
+    test8();
+    func();
     return 0;
 }
